c894e98ec904bbc570da20c9b423afff61535dab
tobymatthew <trobini18@gmail.com>
2021-06-04 13:25:53 +0100

    'updated the contact'

--- src/components/contactList.js
+++ src/components/contactList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ContactCard from './contactCard';
 
 const ContactList= (props)=>{
     
@@ -6,13 +7,7 @@ const ContactList= (props)=>{
     const RenderContactList= props.contact.map((contacts)=>{
         console.log(contacts)
         return(
-            <div className="item">
-                <div className="content">
-                    <div className="header">{contacts.name}</div>
-                    <div>{contacts.email}</div>
-                </div>
-                <i className="trash alternate outline icon"></i>
-            </div>
+           <ContactCard contacts = {contacts}/>
         );
     });
 
